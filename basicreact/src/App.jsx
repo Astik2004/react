d73b205a1782586961dfa3da5f38e9d373493196
Astik <astikyadav4@gmail.com>
2024-07-23 22:43:23 +0530

    REACT

---
+++ basicreact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Astik Yadav React with VITE</h1>
+  )
+}
+
+export default App
---
+++ basicreact1/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Astik Yadav React By Chai aur react</h1>
+  );
+}
+
+export default App;
