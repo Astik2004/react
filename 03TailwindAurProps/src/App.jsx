129a96530565510e3e290850441ea3a579d6bae7
Astik <astikyadav4@gmail.com>
2024-08-02 10:36:36 +0530

    react

--- 03TailwindAurProps/src/App.jsx
+++ 03TailwindAurProps/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Card from './component/card';
+import Bg from './component/bd';
 function App() {
 
   return (
@@ -7,7 +8,7 @@ function App() {
     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
     <Card username="Astik Yadav" />
     <Card username="Ankit Yadav" btntext="Veiw Profile"/>
-
+    <Bg/>
     </>
   )
 }
